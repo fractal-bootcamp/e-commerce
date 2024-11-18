7de64055f01b2cfc4689ea59afae7b75d385beef
Aaditya Desai <aadityadesai09@gmail.com>
2024-11-18 14:27:24 -0500

    created home navbar

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Snack Safari",
+  description: "Your wild adventure into delicious snacks",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} antialiased`}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
