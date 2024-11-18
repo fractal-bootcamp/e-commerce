23dd57b4f662578b35bb1ec907370591857a803d
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-18 12:29:28 -0500

    Initialized express project

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import express from "express";
+const app = express();
+const port = process.env.PORT;
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
