b5e0f513d4fccacb32e344656d369c29e83e2847
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-19 11:46:04 -0500

    Added test for auth controller

--- backend/tests/controllers/controllersAuth.test.ts
+++ backend/tests/controllers/controllersAuth.test.ts
@@ -1,15 +1,18 @@
 import { describe, expect, vi, it, beforeEach } from "vitest";
 import request from "supertest";
 import { app } from "../../server"; // Ensure this path is correct
+import { getIdToken } from "../../firebase/getIdToken";
 
-describe("GET /", () => {
+describe("GET /auth/signup", () => {
   beforeEach(() => {
     vi.clearAllMocks();
   });
 
-  it("should return 200 and Hello World", async () => {
-    const response = await request(app).get("/");
+  it("should return 200 and firebaseId", async () => {
+    const token = await getIdToken();
+    console.log(token);
+    const response = await request(app).get("/auth/signup").set("Authorization", `Bearer ${token}`);
     expect(response.status).toBe(200);
-    expect(response.text).toMatch("Hello World!");
+    // expect(response.text).toMatch("Hello World!");
   });
 });
