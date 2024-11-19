78e201112e26d94737920a8fc7e3900a90517367
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-19 12:07:20 -0500

    Added prisma routes for pulling product info

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { PORT } from "./globals";
 import { verifyFirebaseToken } from "./firebase/middleware";
 import routesAuth from "./routes/routesAuth";
+import routesProduct from "./routes/routesProduct";
 
 export const app = express();
 const cors = require("cors");
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/auth", verifyFirebaseToken, routesAuth);
+app.use("/product", verifyFirebaseToken, routesProduct);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
