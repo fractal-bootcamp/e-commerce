0e5b3c562c511800942763bd7f847994fd436118
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-20 13:23:03 -0500

    Rearranged files for frontend-admin

---
+++ frontend-admin/src/api/apiAuth.ts
@@ -0,0 +1,20 @@
+import { auth } from "../firebase/firebaseConfig";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import axiosClient from "./axiosClient";
+
+export const firebaseAuth = async () => {
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider);
+  const idToken = await result.user.getIdToken();
+
+  const res = await axiosClient({
+    method: "POST",
+    url: "/auth/signup",
+    headers: {
+      Authorization: `Bearer ${idToken}`,
+    },
+  });
+
+  const data = res.data;
+  return data;
+};
