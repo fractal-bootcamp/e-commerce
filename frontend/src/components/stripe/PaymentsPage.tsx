23bc20c2dad69095795ef418406231a55697cda2
mahlen <malink027@gmail.com>
2024-11-20 10:09:11 -0500

    fixed type error in payment intent service

--- frontend/src/components/stripe/PaymentsPage.tsx
+++ frontend/src/components/stripe/PaymentsPage.tsx
@@ -2,8 +2,11 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe, StripeElementsOptions } from '@stripe/stripe-js';
 import CheckoutForm from './CheckoutForm';
 
-// this is a stripe public key
-const stripePromise = loadStripe('pk_test_51QMzu9Cv58AbF4KC0RLhTFUCJDhg1U4HHQHNkpB6boCepcoZtMKSf6p9B1L56xytCLmiTbVsxqvOKUMZERDqu0v700TXkt4SOV');
+if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+  throw new Error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not set');
+}
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 export function PaymentsPage({ paymentOptions }: { paymentOptions: StripeElementsOptions }) {
   return (
