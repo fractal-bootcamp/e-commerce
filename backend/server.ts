c69cf2e91dc7d245f46fbb49aa0618e8784d5c62
mahlen <malink027@gmail.com>
2024-11-19 19:19:45 -0500

    payment route created and corresponding route initialisation check in server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -4,11 +4,19 @@ import { verifyFirebaseToken } from "./firebase/middleware";
 import routesAuth from "./routes/routesAuth";
 import routesProduct from "./routes/routesProduct";
 import routesOrder from "./routes/routesOrder";
-
+import routesPayment from "./routes/routesPayment";
 
 export const app = express();
 const cors = require("cors");
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const paymentIntent = await stripe.paymentIntents.create({
+  amount: 1099,
+  currency: 'usd',
+  automatic_payment_methods: {
+    enabled: true,
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +28,7 @@ app.get("/", (req, res) => {
 app.use("/auth", verifyFirebaseToken, routesAuth);
 app.use("/product", verifyFirebaseToken, routesProduct);
 app.use("/order", verifyFirebaseToken, routesOrder);
+app.use("/payment", verifyFirebaseToken, routesPayment);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
