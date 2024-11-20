c9b654f7d30be0670cf9800904115a8212f7f555
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-20 11:53:08 -0500

    Added controllers for rag queries

--- backend/controllers/controllersRag.ts
+++ backend/controllers/controllersRag.ts
@@ -4,6 +4,7 @@ import type { Request, Response } from "express";
 
 export const ragQuery = withLogging("ragQuery", false, async (req: Request, res: Response) => {
   const { query } = req.body;
+  console.log(query);
   const similarDocuments = await findSimilarDocuments(query, 3);
   res.status(200).json(similarDocuments);
 });
