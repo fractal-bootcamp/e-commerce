c70eb079b23f84e5f256ba02835d7fb3b5257a12
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-22 13:18:19 -0500

    Added hook for protected routes

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import XLoginButton from "@/components/XLoginButton";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <div className="w-full flex justify-center pt-4">
       <XLoginButton />
     </div>
   );
