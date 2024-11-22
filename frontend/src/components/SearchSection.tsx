cd8b72421d9feacd775fcc68093f55441422e2ed
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-22 09:44:25 -0500

    Fixed font import in search section

--- frontend/src/components/SearchSection.tsx
+++ frontend/src/components/SearchSection.tsx
@@ -1,15 +1,15 @@
-import { openSans } from '../app/layout';
+import { openSans } from "@/utils/fonts";
 
 export type SearchSectionProps = {
   query: string;
   setQuery: React.Dispatch<React.SetStateAction<string>>;
-  handleSearch: React.MouseEventHandler<HTMLButtonElement>
-}
+  handleSearch: React.MouseEventHandler<HTMLButtonElement>;
+};
 
 export const SearchSection: React.FC<SearchSectionProps> = ({ query, setQuery }) => {
   return (
     <div>
-      <div className='flex flex-col items-center'>
+      <div className="flex flex-col items-center">
         <input
           className={`input w-full border-2 border-amber-300 ${openSans.className} rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500`}
           // the current value of the query state should be what is displayed in the box
@@ -20,5 +20,5 @@ export const SearchSection: React.FC<SearchSectionProps> = ({ query, setQuery })
         />
       </div>
     </div>
-  )
-}
+  );
+};
