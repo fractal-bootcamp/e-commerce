74576ca81d9a9f09236c67a329ba9f4e699b4d95
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-25 12:19:42 -0500

    Added controller for redis test

---
+++ backend/controllers/controllersRedis.ts
@@ -0,0 +1,31 @@
+import { createHash } from "crypto";
+import { withLogging } from "../utils/withLogging";
+import { redisClient } from "../redis/redisClient";
+import type { Request, Response } from "express";
+
+export const redisTest = withLogging("redisTest", false, async (req: Request, res: Response) => {
+  const { data } = req.body;
+  const cacheKey = createHash("sha256").update(JSON.stringify(data)).digest("hex");
+
+  // Try getting cached data; if it is in the cache, return
+  const cachedData = await redisClient.get(`cache:${cacheKey}`);
+  if (cachedData) {
+    console.log("Cache hit on key: ", cacheKey);
+    console.log("cachedData: ", cachedData);
+    return res.json(JSON.parse(cachedData));
+  }
+
+  // Add a small delay to simulate an expensive operation
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  // If not in cache, fetch from main data source and store in Redis for 60 min
+  const newData = {
+    id: "123",
+    country: "US",
+    category: "Clothing",
+  };
+  await redisClient.setex(`cache${cacheKey}`, 3600, JSON.stringify(newData));
+  res.json(newData);
+
+  res.status(200).json();
+});
--- backend/redis/redisClient.ts
+++ backend/redis/redisClient.ts
@@ -1,4 +1,4 @@
 import Redis from "ioredis";
 import { REDIS_URL } from "../globals";
 
-export const redis = new Redis(REDIS_URL);
+export const redisClient = new Redis(REDIS_URL);
