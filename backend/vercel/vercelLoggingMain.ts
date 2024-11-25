8a7e7d6856025b5ad131f9044ff9e5837f0eff05
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-25 17:28:46 -0500

    Final changes

--- backend/vercel/vercelLoggingMain.ts
+++ backend/vercel/vercelLoggingMain.ts
@@ -1,4 +1,4 @@
-import { NODEMAILER_EMAIL_PASSWORD, NODEMAILER_EMAIL_USER, VERCEL_TOKEN } from "../globals";
+import { VERCEL_TOKEN } from "../globals";
 import { sendEmail } from "../notifications/nodemailer";
 import type { VercelDeployment } from "../types/notifications";
 import { getVercelFailedDeployments } from "./getVercelFailedDeployments";
@@ -21,8 +21,6 @@ const vercelLoggingMain = async (): Promise<void> => {
     ]);
 
   const allDeployments = [...userDeployments, ...adminDeployments];
-  console.log(NODEMAILER_EMAIL_PASSWORD);
-  console.log(NODEMAILER_EMAIL_USER);
 
   await Promise.all(
     emailRecipients.flatMap((recipient) =>
