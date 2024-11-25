657c310877d3249f80640869881d6f5719c2f4a5
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-25 15:32:17 -0500

    Added script for pulling vercel error logs

--- backend/vercel/getVercelDeployments.ts
+++ backend/vercel/getVercelDeployments.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-export const getVercelDeployments = async (limit = 1, vercelToken: string) => {
+export const getVercelDeployments = async (limit = 1, projectName: string, vercelToken: string) => {
   const res = await axios.request({
     method: "GET",
-    url: `http://api.vercel/com/v6/deployments?limit=${limit}`,
+    url: `https://api.vercel.com/v6/deployments?limit=${limit}`,
     headers: {
       Authorization: `Bearer ${vercelToken}`,
     },
   });
   const data = res.data.deployments;
-  return data;
+  const dataParsed = data.filter((d: any) => d.name === projectName);
+  return dataParsed;
 };
