2f3788241ea7fe892046bc767620b9360aabc25c
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-25 15:43:39 -0500

    Added nodemailer script

--- backend/globals.ts
+++ backend/globals.ts
@@ -1,3 +1,9 @@
 // Express
 export const PORT = process.env.PORT;
+
+// Redis
 export const REDIS_URL = process.env.REDIS_URL;
+
+// Nodemail
+export const NODEMAILER_EMAIL_USER = process.env.NODEMAILER_EMAIL_USER;
+export const NODEMAILER_EMAIL_PASSWORD = process.env.NODEMAILER_EMAIL_PASSWORD;
